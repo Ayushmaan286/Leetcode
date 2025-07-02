5c608c2f8915b44ca3822d2821a32c37e2b2a09c
Ayushmaan286 <ayushmaansingh286@gmail.com>
2025-07-02 19:30:39 +0530

    updated again

--- missing number.java
+++ missing number.java
@@ -1,10 +1,4 @@
-/*Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
- * 
- * https://leetcode.com/problems/missing-number/description/{leetcode link}
- */
-
-
-
+/*Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.*/
 
 
 class Solution {
