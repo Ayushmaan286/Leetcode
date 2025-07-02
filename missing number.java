9ec8117c73afcf3587d381fab861b641774aa7c1
Ayushmaan286 <ayushmaansingh286@gmail.com>
2025-07-02 19:48:26 +0530

    hint for the problem

--- missing number.java
+++ missing number.java
@@ -1,5 +1,7 @@
 /*Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.*/
 
+/* array ka sum calculate kro , then 0 se leke array ki length tk ka sum find kro fir dono ko subtract krdo , you will get the required missing number */
+
 
 class Solution {
     public int missingNumber(int[] nums) {
